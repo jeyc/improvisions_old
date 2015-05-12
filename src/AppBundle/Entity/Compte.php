54970ec74c34a67eedf299c4f0be2f9d5ee733d4
biouche <jerome.eychenne@gmail.com>
2015-05-12 22:33:24 +0200

    Création Entity "Compte" avec :
    - deux champs : code et libellé
    - annotations de mapping ORM
    - getters et setters
    - contraintes de validations avec messages
    - traductions des messages de validation en locale fr

---
+++ src/AppBundle/Entity/Compte.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Compte
+ *
+ * @ORM\Table(name="improvisions.comptes", uniqueConstraints={@ORM\UniqueConstraint(columns={"code"})})
+ * @ORM\Entity
+ * @UniqueEntity(fields="code", message="compte.code.unique")
+ */
+class Compte
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=6)
+     * @Assert\NotBlank(message="compte.code.not_blank")
+	 * @Assert\Type(type="digit", message="compte.code.type_digit")
+	 * @Assert\Length(min="6", max="6", exactMessage="compte.code.length")
+	 * @Assert\Regex(pattern="^[1-9][0-9]{5,5}^", message="compte.code.regex")
+	*/
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=255)
+	 * @Assert\NotBlank(message="compte.libelle.not_blank")
+     */
+    private $libelle;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Compte
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     * @return Compte
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string 
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+	
+}
