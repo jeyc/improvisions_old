c0bca0dabb1511e10dcfafe2780fa5a7a694d613
biouche <jerome.eychenne@gmail.com>
2015-05-20 17:37:12 +0200

    Ajout entity Année

---
+++ src/AppBundle/Entity/Annee.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Annee
+ *
+ * @ORM\Table("improvisions.annees")
+ * @ORM\Entity
+ * @UniqueEntity(fields={"annee"}, message="annee.annee.unique")
+ */
+class Annee
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="annee", type="string", length=4)
+	 * @Assert\NotBlank(message="annee.annee.not_blank")
+	 * @Assert\Regex(pattern="/20[1-9][0-9]/", message="annee.annee.regex")
+     */
+    private $annee;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set annee
+     *
+     * @param string $annee
+     * @return Annee
+     */
+    public function setAnnee($annee)
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    /**
+     * Get annee
+     *
+     * @return string 
+     */
+    public function getAnnee()
+    {
+        return $this->annee;
+    }
+}
